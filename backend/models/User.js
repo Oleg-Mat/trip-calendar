5c3c26f4458fd366b977e9270a466fd9291ba37f
Maklay <okunev.js@gmail.com>
2019-09-12 17:08:40 +0300

    worked front & back

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,12 +3,18 @@ const db = require('mongoose');
 const UserSchema = new db.Schema({
   createDate: { type: Date, default: new Date() },
   googleId: db.Schema.Types.Mixed,
+  firstName: String,
+  lastName: String,
+  fullName: String,
 });
-UserSchema.statics.findOrCreate = async function (id, cb) {
-  let user = await this.findOne({ googleId: id.googleId });
+UserSchema.statics.findOrCreate = async function (profile, cb) {
+  let user = await this.findOne({ googleId: profile.id });
   if (!user) {
     user = new db.model('User', UserSchema)({
-      googleId: id.googleId,
+      googleId: profile.id,
+      firstName: profile.name.familyName,
+      lastName: profile.name.givenName,
+      fullName: profile.displayName,
     });
     await user.save();
   }
