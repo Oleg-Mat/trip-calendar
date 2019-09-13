2c3e69a7db03c9d6b583662b3dd0f43412afd472
Shamil Gadzhiev <shimunic@gmail.com>
2019-09-13 17:54:42 +0300

    wip

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,7 +7,6 @@ const UserSchema = new db.Schema({
   lastName: String,
   fullName: String,
   locale: String,
-  avatar: String,
   company: String,
   email: String,
   website: String,
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -19,8 +19,21 @@ router.get('/logout', auth, async (req, res) => {
 
 router.get('/user/:_id', auth, async (req, res) => {
   const user = await User.findOne({ _id: req.params._id });
-
+  console.log(req.params);
+  
   res.json(user);
+});
+
+router.post('/user/:_id/updateprofile', auth, async (req, res) => {
+  const changedUser = {};
+  changedUser.firstName = req.body.firstName;
+  changedUser.lastName = req.body.lastName;
+  changedUser.photo = req.body.photo;
+  changedUser.company = req.body.company;
+  changedUser.website = req.body.website;
+  await User.findByIdAndUpdate({ _id: req.params._id }, { $set: { changedUser } }, { new: true }, (err, user) => {
+    if (err) { console.log(err); }
+  });
 });
 
 router.get('/timeline/:_id', auth, async (req, res) => {
@@ -28,33 +41,36 @@ router.get('/timeline/:_id', auth, async (req, res) => {
 
   res.json(user);
 });
-router.get('/timelineonperiod',  async (req, res) => {
-  let { dateStart:InputStart, dateEnd:InputEnd, place:InputPlace } = req.query;
-  
-      InputEnd = new Date(InputEnd);
-      InputStart = new Date(Date.parse(InputStart));
+router.get('/timelineonperiod', async (req, res) => {
+  let { dateStart: InputStart, dateEnd: InputEnd, place: InputPlace } = req.query;
 
- const usersWithTimeline = await Timeline.find({
-   $and:[
-     {place:InputPlace} , 
-     {$or:[{dateStart:{$lte:InputEnd}},
-       {dateEnd:{$gte:InputStart}}]
-  }]
- 
-}).populate("userId");
+  InputEnd = new Date(InputEnd);
+  InputStart = new Date(Date.parse(InputStart));
 
-res.json(usersWithTimeline);
+  const usersWithTimeline = await Timeline.find({
+    $and: [
+      { place: InputPlace },
+      {
+        $or: [{ dateStart: { $lte: InputEnd } },
+          { dateEnd: { $gte: InputStart } }],
+      }],
+
+  }).populate('userId');
+
+  res.json(usersWithTimeline);
 });
 
 
-router.post('/timeline', (req, res) => {
-  const { userId, dateStart, dateEnd, place } = req.body;
+router.post('/timeline', async (req, res) => {
+  const {
+    userId, dateStart, dateEnd, place,
+  } = req.body;
   const timeline = new Timeline({
     userId,
     dateStart,
     dateEnd,
     place,
   });
-  timeline.save();
+  await timeline.save();
 });
 module.exports = router;
